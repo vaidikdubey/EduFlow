1021000ee106ac801d70f908d2bca7cd6c0ae9cf
Vaidik Dubey <vaidik26dubey@gmail.com>
2026-01-20 16:32:53 +0530

    Basic express backend setup

---
+++ backend/src/index.js
@@ -0,0 +1,10 @@
+import app from "./app.js";
+import dotenv from "dotenv";
+
+dotenv.config({
+  path: "./.env",
+});
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, console.log(`Backend is running on PORT: ${PORT}`));
