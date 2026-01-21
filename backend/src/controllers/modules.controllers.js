0be55cf249d84541dbc5dfec2fb9121a0e6253ce
Vaidik Dubey <vaidik26dubey@gmail.com>
2026-01-21 23:07:58 +0530

    Added get all modules controller

--- backend/src/controllers/modules.controllers.js
+++ backend/src/controllers/modules.controllers.js
@@ -3,7 +3,30 @@ import { asyncHandler } from "../utils/async-handler.js";
 import { ApiError } from "../utils/api-error.js";
 import { ApiResponse } from "../utils/api-response.js";
 
-const getAllModules = asyncHandler(async (req, res) => {});
+const getAllModules = asyncHandler(async (req, res) => {
+  const { courseId } = req.params;
+
+  if (!courseId) throw new ApiError(404, "Course id is required");
+
+  const allModules = await db.module.findMany({
+    where: { courseId },
+    select: {
+      id: true,
+      title: true,
+      order: true,
+      courseId: true,
+      _count: {
+        select: {
+          lessons: true,
+          quiz: true,
+        },
+      },
+    },
+    orderBy: { order: "asc" },
+  });
+
+  res.status(200).json(new ApiResponse(200, allModules, "Modules fetched"));
+});
 
 const getModuleById = asyncHandler(async (req, res) => {});
 
