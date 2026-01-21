4d7b929a709c77c529a201982714bd2644a2dfb4
Vaidik Dubey <vaidik26dubey@gmail.com>
2026-01-21 22:59:04 +0530

    Added module routes

--- backend/src/routes/modules.routes.js
+++ backend/src/routes/modules.routes.js
@@ -1,6 +1,22 @@
 import { Router } from "express";
+import { isLoggedIn, checkAdmin } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
+//Public routes
+router.route("/getAll/:courseId").get(isLoggedIn, getAllModules);
+
+router.route("/getModule/:id").get(isLoggedIn, getModuleById);
+
+router.route("/getModuleProgress/:id").get(isLoggedIn, getModuleProgress);
+
+//Instructor and admin routes
+router.route("/create").post(isLoggedIn, checkAdmin, createModule);
+
+router.route("/update/:id").patch(isLoggedIn, checkAdmin, updateModule);
+
+router.route("/delete/:id").delete(isLoggedIn, checkAdmin, deleteModule);
+
+router.route("/getModuleStats/:id").get(isLoggedIn, checkAdmin, getModuleStats);
 
 export default router;
