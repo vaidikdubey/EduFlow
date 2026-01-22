92c5cc3065540ded556f1a209cc1d75c537218ef
Vaidik Dubey <vaidik26dubey@gmail.com>
2026-01-22 22:01:10 +0530

    Added enrollment routes

--- backend/src/routes/enrollments.routes.js
+++ backend/src/routes/enrollments.routes.js
@@ -1,6 +1,27 @@
 import { Router } from "express";
+import { checkAdmin, isLoggedIn } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
+router.route("/enroll/:courseId").post(isLoggedIn, enrollInCourse);
+
+//Razorpay payment route
+router.route("/webhook/razorpay").post(handleRazorpayWebhook);
+
+router.route("/enrolled/:courseId").get(isLoggedIn, checkEnrollmentStatus);
+
+router.route("/myEnrollments").get(isLoggedIn, getMyEnrollments);
+
+router.route("/completed/:id").patch(isLoggedIn, markCourseCompleted);
+
+router.route("/cancel/:courseId").delete(isLoggedIn, cancelEnrollment);
+
+router.route("/getEnrollment/:courseId").get(isLoggedIn, getEnrollmentById);
+
+router.route("/certificate/:courseId").post(isLoggedIn, getCourseCertificate);
+
+router
+  .route("/getAll/:courseId")
+  .get(isLoggedIn, checkAdmin, getCourseEnrollments);
 
 export default router;
