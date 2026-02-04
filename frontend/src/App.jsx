45b40d611ea07bee7747969f88e78e8dff358d05
Vaidik Dubey <vaidik26dubey@gmail.com>
2026-02-04 18:04:17 +0530

    Added basic router structuring and axios, zod libs

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,9 +3,6 @@ import "./App.css";
 function App() {
     return (
         <>
-            <h1>
-                Welcome to <b>EduFlow</b>. Page under construction
-            </h1>
         </>
     );
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { HeroUIProvider } from "@heroui/react";
+import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
-        <HeroUIProvider>
-            <App />
-        </HeroUIProvider>
+        <BrowserRouter>
+            <HeroUIProvider>
+                <App />
+            </HeroUIProvider>
+        </BrowserRouter>
     </StrictMode>,
 );
