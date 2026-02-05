d5b3fd3c8818ab0a3cd779380ad3efe105ff8e19
Vaidik Dubey <vaidik26dubey@gmail.com>
2026-02-05 17:41:16 +0530

    Restarting frontend due to severe component ui issues

--- frontend/src/pages/ResetPasswordPage.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const ResetPasswordPage = () => {
-  return (
-    <div>ResetPasswordPage</div>
-  )
-}
--- frontend/src/pages/VerifyEmailPage.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const VerifyEmailPage = () => {
-  return (
-    <div>VerifyEmailPage</div>
-  )
-}
