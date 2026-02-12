92f9048a0d5a5d948a15a22b827ed711c06bd38b
Vaidik Dubey <vaidik26dubey@gmail.com>
2026-02-12 19:23:10 +0530

    Added loader for getting courses early return component

--- frontend/src/pages/courses/AllCoursesPage.jsx
+++ frontend/src/pages/courses/AllCoursesPage.jsx
@@ -1,5 +1,6 @@
-import { useCourseStore } from "@/stores/useCourseStore";
 import React, { useEffect } from "react";
+import { useCourseStore } from "@/stores/useCourseStore";
+import { Loader,  } from "lucide-react";
 
 export const AllCoursesPage = () => {
     const { isGettingAllCourses, allCourses, getAllCourses } = useCourseStore();
@@ -8,5 +9,19 @@ export const AllCoursesPage = () => {
         getAllCourses();
     }, []);
 
-    return <div>AllCoursesPage</div>;
+    console.log("All courses: ", allCourses);
+
+    if (isGettingAllCourses) {
+        return (
+            <div className="h-full flex items-center justify-center">
+                <Loader className="animate-spin text-foreground" />
+            </div>
+        );
+    }
+
+    return (
+        <div className="h-full w-full flex flex-col items-center justify-center">
+            <h1>All courses </h1>
+        </div>
+    );
 };
