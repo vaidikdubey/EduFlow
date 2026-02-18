4905b3a957a7d296802707fdacef783073affe2e
Vaidik Dubey <vaidik26dubey@gmail.com>
2026-02-18 21:17:09 +0530

    Added course fetching on load and loader for course home page

--- frontend/src/pages/courses/CourseHomePage.jsx
+++ frontend/src/pages/courses/CourseHomePage.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { useCourseStore } from "@/stores/useCourseStore";
+import { Loader } from "lucide-react";
+import { useParams } from "react-router-dom";
 
 export const CourseHomePage = () => {
-  return (
-    <div>CourseHomePage</div>
-  )
-}
+    const { id } = useParams();
+
+    const { getCourseById, isGettingCourse, fetchedCourse } = useCourseStore();
+
+    useEffect(() => {
+        getCourseById(id);
+    }, [id]);
+
+    if (isGettingCourse) {
+        return (
+            <div className="h-full flex items-center justify-center">
+                <Loader className="animate-spin text-foreground" />
+            </div>
+        );
+    }
+
+    return <div>CourseHomePage</div>;
+};
