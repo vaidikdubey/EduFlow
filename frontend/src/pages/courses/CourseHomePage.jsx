6ae1b18e5f8df047cfa04689412f712f6398e3ae
Vaidik Dubey <vaidik26dubey@gmail.com>
2026-02-18 23:03:22 +0530

    Added modules list, with sorting optimization.

--- frontend/src/pages/courses/CourseHomePage.jsx
+++ frontend/src/pages/courses/CourseHomePage.jsx
@@ -1,22 +1,33 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useMemo } from "react";
 import { useCourseStore } from "@/stores/useCourseStore";
-import { ArrowLeft, Loader } from "lucide-react";
+import { ArrowLeft, ArrowRight, Loader } from "lucide-react";
 import { Link, useParams } from "react-router-dom";
 import { ReadMore } from "@/utils/ReadMore";
 import { timeAgo } from "@/utils/timeAgo";
+import { useModuleStore } from "@/stores/useModuleStore";
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
 
 export const CourseHomePage = () => {
     const { id } = useParams();
 
     const { getCourseById, isGettingCourse, fetchedCourse } = useCourseStore();
 
+    const { isGettingAllModules, allModules, getAllModules } = useModuleStore();
+
     useEffect(() => {
         getCourseById(id);
+        getAllModules(id);
     }, [id]);
 
-    console.log("Fetched course: ", fetchedCourse?.data);
+    //Optimized modules sorting
+    const sortedModules = useMemo(() => {
+        if (!allModules || allModules?.length === 0) return [];
 
-    if (isGettingCourse) {
+        return [...allModules.data].sort((a, b) => a.order - b.order);
+    }, [allModules]);
+
+    if (isGettingCourse || isGettingAllModules) {
         return (
             <div className="h-full flex items-center justify-center">
                 <Loader className="animate-spin text-foreground" />
@@ -24,64 +35,107 @@ export const CourseHomePage = () => {
         );
     }
 
+    console.log("All modules: ", allModules?.data);
+
     return (
         <div className="h-full w-full flex flex-col">
             {/* NavBar */}
-            <div className="flex items-center gap-2">
-                <Link to={"/"} className="hidden md:block">
-                    <ArrowLeft size={18} />
-                </Link>
-                <div className="h-full w-full flex flex-col justify-center items-start md:pl-2">
-                    <h1 className="text-3xl font-bold hover:underline underline-offset-4 cursor-pointer hover:text-foreground/95">
-                        {fetchedCourse?.data?.title}
-                    </h1>
-                    {fetchedCourse?.data?.description && (
-                        <ReadMore text={fetchedCourse?.data?.description} />
-                    )}
+            <div>
+                <div className="flex items-center gap-2">
+                    <Link to={"/"} className="hidden md:block">
+                        <ArrowLeft size={18} />
+                    </Link>
+                    <div className="h-full w-full flex flex-col justify-center items-start md:pl-2">
+                        <h1 className="text-3xl font-bold hover:underline underline-offset-4 cursor-pointer hover:text-foreground/95">
+                            {fetchedCourse?.data?.title}
+                        </h1>
+                        {fetchedCourse?.data?.description && (
+                            <ReadMore text={fetchedCourse?.data?.description} />
+                        )}
+                    </div>
                 </div>
-            </div>
-            <div className="w-full flex flex-col gap-1 md:px-8 py-2">
-                <div className="flex flex-col md:flex-row justify-between md:items-center">
-                    <h5>
-                        <span className="font-semibold">Author: </span>
-                        {fetchedCourse?.data?.createdBy?.name}
-                    </h5>
-                    <p>
-                        <span className="font-semibold">Added: </span>
-                        {timeAgo(fetchedCourse?.data?.createdAt)}
-                    </p>
+                <div className="w-full flex flex-col gap-1 md:px-8 py-2">
+                    <div className="flex flex-col md:flex-row justify-between md:items-center">
+                        <h5>
+                            <span className="font-semibold">Author: </span>
+                            {fetchedCourse?.data?.createdBy?.name}
+                        </h5>
+                        <p>
+                            <span className="font-semibold">Added: </span>
+                            {timeAgo(fetchedCourse?.data?.createdAt)}
+                        </p>
+                    </div>
+                    <div className="flex gap-1">
+                        <span className="font-semibold">Instructors: </span>{" "}
+                        {fetchedCourse?.data?.instructors && (
+                            <ReadMore
+                                text={fetchedCourse?.data?.instructors
+                                    .map((ins) => ins.name)
+                                    .join(", ")}
+                                maxLen={50}
+                            />
+                        )}
+                    </div>
                 </div>
-                <p>
-                    <span className="font-semibold">Instructors: </span>{" "}
-                    {fetchedCourse?.data?.instructors && (
-                        <ReadMore
-                            text={fetchedCourse?.data?.instructors
-                                .map((ins) => ins.name)
-                                .join(", ")}
-                            maxLen={50}
-                        />
-                    )}
-                </p>
+
+                {/* Course Statistics */}
+                <div className="flex flex-col md:px-8 py-2 border border-dotted rounded-xl">
+                    <h6 className="font-semibold mx-auto pb-2 underline underline-offset-2">
+                        Course Stats
+                    </h6>
+                    <div className="flex justify-between items-center cursor-default">
+                        <p>
+                            <span>Modules: </span>
+                            {fetchedCourse?.data?._count?.modules}
+                        </p>
+                        <p>
+                            <span>Enrollments: </span>
+                            {fetchedCourse?.data?._count?.enrollments}
+                        </p>
+                        <p>
+                            <span>Quizzes: </span>
+                            {fetchedCourse?.data?._count?.quizzes}
+                        </p>
+                    </div>
+                </div>
             </div>
 
-            {/* Course Statistics */}
-            <div className="flex flex-col md:px-8 py-2 border border-dotted rounded-xl">
-                <h6 className="font-semibold mx-auto pb-2 underline underline-offset-2">
-                    Course Stats
-                </h6>
-                <div className="flex justify-between items-center cursor-default">
-                    <p>
-                        <span>Modules: </span>
-                        {fetchedCourse?.data?._count?.modules}
-                    </p>
-                    <p>
-                        <span>Enrollments: </span>
-                        {fetchedCourse?.data?._count?.enrollments}
-                    </p>
-                    <p>
-                        <span>Quizzes: </span>
-                        {fetchedCourse?.data?._count?.quizzes}
-                    </p>
+            {/* Modules Section */}
+            <div className="flex-1 h-full w-full px-8 py-2 no-scroll">
+                <h2 className="font-bold text-xl">Modules: </h2>
+                <div className="h-full w-full border border-dashed my-1 p-4 rounded-2xl flex flex-col gap-3 overflow-y-auto no-scroll">
+                    {sortedModules?.map((module) => {
+                        return (
+                            <div
+                                key={module.id}
+                                className="w-full p-2 border-2 rounded-xl flex justify-between items-center"
+                            >
+                                <div>
+                                    <h3 className="font-bold">
+                                        <span>{module.order}. </span>{" "}
+                                        {module.title}
+                                    </h3>
+                                    <p>
+                                        <span className="font-semibold">
+                                            Lessons:{" "}
+                                        </span>{" "}
+                                        {module._count.lessons}
+                                    </p>
+                                    <p>
+                                        <span className="font-semibold">
+                                            Quiz:{" "}
+                                        </span>{" "}
+                                        {module._count.quiz}
+                                    </p>
+                                </div>
+                                <Button variant="icon" asChild>
+                                    <Link to={`/module/get/${module.id}`}>
+                                        <ArrowRight />
+                                    </Link>
+                                </Button>
+                            </div>
+                        );
+                    })}
                 </div>
             </div>
         </div>
