15e9e22cab93786ce3d8a856d19b51f6bdd9f405
Vaidik Dubey <vaidik26dubey@gmail.com>
2026-02-24 19:46:33 +0530

    Updated create lessons page.

--- frontend/src/pages/lessons/CreateLesson.jsx
+++ frontend/src/pages/lessons/CreateLesson.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import React from "react";
+import { useLessonStore } from "@/stores/useLessonStore";
+import { useParams } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 export const CreateLesson = () => {
-  return (
-    <div>CreateLesson</div>
-  )
-}
+    const { id } = useParams();
+    
+    const { createLesson, isCreatingLesson, createdLesson } = useLessonStore();
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+        setValue,
+    } = useForm({
+        resolver: zodResolver()
+    })
+
+    return <div>CreateLesson</div>;
+};
