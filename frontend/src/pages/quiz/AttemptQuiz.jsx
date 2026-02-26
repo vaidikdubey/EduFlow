280fd9088402b8f8c9771109b46a1300c18ca8f0
Vaidik Dubey <vaidik26dubey@gmail.com>
2026-02-26 14:18:39 +0530

    Added get quiz by id method in quiz store.

--- frontend/src/pages/quiz/AttemptQuiz.jsx
+++ frontend/src/pages/quiz/AttemptQuiz.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 export const AttemptQuiz = () => {
+  const { id } = useParams();
+
   return (
     <div>AttemptQuiz</div>
   )
--- frontend/src/stores/useQuizStore.js
+++ frontend/src/stores/useQuizStore.js
@@ -5,6 +5,8 @@ import toast from "react-hot-toast";
 export const useQuizStore = create((set) => ({
     isGettingQuizForModule: false,
     allQuizForModule: [],
+    isGettingQuiz: false,
+    quizById: null,
 
     getAllQuizForModule: async (id) => {
         set({ isGettingQuizForModule: true });
@@ -14,10 +16,29 @@ export const useQuizStore = create((set) => ({
 
             set({ allQuizForModule: res.data });
         } catch (error) {
+            console.error("Error fetching quizzes", error);
+            toast.error(
+                error.response.data.message || "Error fetching quizzes",
+            );
+        } finally {
+            set({ isGettingQuizForModule: false });
+        }
+    },
+
+    getQuizById: async (id) => {
+        set({ isGettingQuiz: true });
+
+        try {
+            const res = await axiosInstance.get(`/quiz/getQuiz/${id}`);
+
+            set({ quizById: res.data });
+
+            toast.success(res.message || "Quiz fetched");
+        } catch (error) {
             console.error("Error fetching quiz", error);
             toast.error(error.response.data.message || "Error fetching quiz");
         } finally {
-            set({ isGettingQuizForModule: false });
+            set({ isGettingQuiz: false });
         }
     },
 }));
