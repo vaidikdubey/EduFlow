0f0e773bf85e8d6c63fe4a05d7c46fe19c0339a9
Vaidik Dubey <vaidik26dubey@gmail.com>
2026-02-27 14:10:28 +0530

    Added get module by id method in module store.

--- frontend/src/stores/useModuleStore.js
+++ frontend/src/stores/useModuleStore.js
@@ -5,6 +5,8 @@ import toast from "react-hot-toast";
 export const useModuleStore = create((set) => ({
     isGettingAllModules: false,
     allModules: [],
+    isGettingModule: false,
+    moduleById: null,
 
     getAllModules: async (id) => {
         set({ isGettingAllModules: true });
@@ -18,6 +20,23 @@ export const useModuleStore = create((set) => ({
             toast.error(error.response.data.message || "Error getting modules");
         } finally {
             set({ isGettingAllModules: false });
+        }
+    },
+
+    getModuleById: async (id) => {
+        set({ isGettingModule: true });
+
+        try {
+            const res = await axiosInstance.get(`/module/getModule/${id}`);
+
+            set({ moduleById: res.data });
+
+            toast.success(res.message || "Module found");
+        } catch (error) {
+            console.error("Error getting module by id", error);
+            toast.error(error.response.data.message || "Error getting module");
+        } finally {
+            set({ isGettingModule: false });
         }
     },
 }));
