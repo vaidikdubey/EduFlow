90bee7201a8cf0e0a73f35b197048f2d1f24f633
Vaidik Dubey <vaidik26dubey@gmail.com>
2026-03-05 15:36:54 +0530

    Added input field for quiz name in create quiz component

--- frontend/src/pages/quiz/CreateQuizPage.jsx
+++ frontend/src/pages/quiz/CreateQuizPage.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import React from "react";
+import { Input } from "@/components/ui/input";
+import { cn } from "@/lib/utils";
 
 export const CreateQuizPage = () => {
-  return (
-    <div>CreateQuizPage</div>
-  )
-}
+    return (
+        <div className="h-full w-full max-w-6xl mx-auto border-l-2 border-r-2 px-5">
+            <Input
+                placeholder="Enter quiz title"
+                className={cn(
+                    "placeholder:text-lg placeholder:text-muted-foreground",
+                )}
+            />
+        </div>
+    );
+};
